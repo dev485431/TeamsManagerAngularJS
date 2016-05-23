75eb9d0ae4c1f8c89962f8259ad91d6db03ea272
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-05-23 15:53:48 +0200

    Inital changes #1

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -7,6 +7,21 @@ config(function config($stateProvider) {
     $stateProvider
         .state('home', {
             url: '^/home',
+            // views: {
+            //     'search': {
+            //         templateUrl: 'search/search.tpl.html',
+            //         controller: 'SearchCtrl'
+            //     },
+            //     'choose': {
+            //         templateUrl: 'choose/choose.tpl.html',
+            //         controller: 'ChooseCtrl'
+            //     },
+            //     'teams': {
+            //         templateUrl: 'teams/teams.tpl.html',
+            //         controller: 'TeamsCtrl'
+            //     }
+            // },
+
             controller: 'HomeCtrl',
             templateUrl: 'home/home.tpl.html'
         });
