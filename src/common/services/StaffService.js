ef4b401d66a6cb3be34a61903b8503fe6f7da1f8
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-05-30 15:08:12 +0200

    Proper hierarchy of views! #2 - accordion for staff details;

--- src/common/services/StaffService.js
+++ src/common/services/StaffService.js
@@ -1,16 +1,18 @@
 angular.module('awesome-app.common.services')
     .factory('StaffService', function ($http) {
 
-        var staffService = {
-            getStaff: function () {
-                var promise = $http.get('src/common/datasource/staff.json.js').then(function (response) {
-                    console.log(response);
-                    return response.data;
-                });
+        var dataUrl = 'src/common/datasource/staff.json.js',
 
-                return promise;
-            }
-        };
+            staffService = {
+                getStaff: function () {
+                    var promise = $http.get(dataUrl).then(function (response) {
+                        console.log(response.data);
+                        return response.data;
+                    });
+
+                    return promise;
+                }
+            };
         return staffService;
 
     });
