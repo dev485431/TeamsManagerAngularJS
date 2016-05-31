7c86393ffd3d23e722833f42700afe17d6b3e89f
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-05-31 14:16:31 +0200

    Tabs / list - sorting and filtering for whole data; auto adjustment of number of pages;

--- src/app/tabs/list/listCtrl.js
+++ src/app/tabs/list/listCtrl.js
@@ -1,31 +1,31 @@
 'use strict';
 
 angular.module('awesome-app.tabs')
-    .controller('ListCtrl', function ($scope, StaffService) {
-
-        $scope.sortType = 'name';
-        $scope.sortReverse = false;
-        $scope.searchTerm = '';
+    .controller('ListCtrl', function ($scope, $filter, StaffService) {
 
         $scope.currentPage = 1;
         $scope.itemsPerPage = 10;
         $scope.maxSize = 8;
         $scope.boundaryLinks = true;
 
+        $scope.sortType = 'name';
+        $scope.sortReverse = false;
+        $scope.searchTerm = '';
+
         StaffService.getStaff()
             .then(function (data) {
                 $scope.totalItems = data.length;
 
-                $scope.$watch('currentPage + itemsPerPage', function () {
+                $scope.$watch('currentPage + searchTerm + sortReverse', function () {
                     var begin = (($scope.currentPage - 1) * $scope.itemsPerPage),
                         end = begin + $scope.itemsPerPage;
 
-                    $scope.pageData = data.slice(begin, end);
+                    var sortedData = $filter('orderBy')(data, $scope.sortType, $scope.sortReverse);
+                    var filteredData = $filter('filter')(sortedData, $scope.searchTerm);
+                    $scope.totalItems = filteredData.length || sortedData.length;
+                    $scope.pageData = filteredData.slice(begin, end);
                 });
+
             });
 
-        $scope.setPage = function (pageNo) {
-            $scope.currentPage = pageNo;
-        };
-        
     });
