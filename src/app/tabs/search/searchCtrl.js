981c6d24b5677ac7d26f2aa52e2ae5b05f87b7b4
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-06-06 15:35:24 +0200

    Feature: search matches any word and any word order;

--- src/app/tabs/search/searchCtrl.js
+++ src/app/tabs/search/searchCtrl.js
@@ -1,9 +1,26 @@
 'use strict';
 
-angular.module('awesome-app.tabs').
+angular.module('awesome-app.tabs')
+    .controller('SearchCtrl', function ($scope, $filter, StaffDataService) {
 
-controller('SearchCtrl', function ($scope) {
-    
+        $scope.numbers = [1, 2, 3, 5, 6];
 
+        $scope.addTag = function (item) {
+            console.log('Adding tag: ' + item);
+        };
 
-});
+        StaffDataService.getStaff()
+            .then(function (data) {
+                $scope.staffData = data;
+            });
+
+        $scope.matchAnyWords = function (searchText) {
+            var results = $scope.staffData;
+            var searchWordsArray = searchText ? searchText.toLowerCase().split(/\s+/) : [];
+            for (var i = 0; i < searchWordsArray.length; i++) {
+                results = $filter('filter')(results, searchWordsArray[i]);
+            }
+            return results;
+        };
+
+    });
